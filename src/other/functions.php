70843ad487b7cb4f8ee58c7c03f1db4bfe677a87
Nikita Suchkov <suchkov8520@gmail.com>
2023-06-17 07:39:19 +0300

    Git init

---
+++ src/other/functions.php
@@ -0,0 +1,149 @@
+<?php
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wp_shortlink_wp_head');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10);
+remove_action('wp_head', 'parent_post_rel_link', 10);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'pagenavi_css');
+remove_action('wp_head', 'wp_resource_hints', 2);
+remove_action('wp_head', 'rest_output_link_wp_head');
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_head', 'profile_link');
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('admin_print_styles', 'print_emoji_styles');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_filter('the_content_feed', 'wp_staticize_emoji');
+remove_filter('comment_text_rss', 'wp_staticize_emoji');
+remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
+remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
+remove_filter('get_the_excerpt', 'wp_trim_excerpt', 10, 2);
+remove_filter('the_excerpt', 'wptexturize');
+remove_filter('the_excerpt', 'convert_smilies');
+remove_filter('the_excerpt', 'convert_chars');
+remove_filter('the_excerpt', 'wpautop');
+remove_filter('the_excerpt', 'shortcode_unautop');
+remove_filter('the_excerpt', 'wp_filter_content_tags');
+remove_filter('the_excerpt', 'wp_replace_insecure_home_url');
+
+add_filter('wp_robots', 'wp_robots_max_image_preview_large');
+add_filter('show_admin_bar', '__return_false');
+add_filter('nav_menu_css_class', '__return_empty_array');
+add_filter('wpcf7_load_css', '__return_false');
+
+add_filter('xmlrpc_methods', function ($methods) {
+   unset($methods['pingback.ping']);
+   unset($methods['pingback.extensions.getPingbacks']);
+   return $methods;
+});
+
+add_filter('wp_headers', function ($headers) {
+   unset($headers['X-Pingback']);
+   return $headers;
+});
+
+add_filter('rest_authentication_errors', function ($result) {
+   if (empty($result)) {
+      if (!is_user_logged_in())
+         return new WP_Error('rest_not_logged_in', 'You are not currently logged in.', array('status' => 401));
+   }
+   return $result;
+});
+
+
+add_action('after_setup_theme', function () {
+   if (!current_user_can('administrator') && !is_admin()) {
+      show_admin_bar(false);
+   }
+});
+
+add_action('wp_head', function () {
+?>
+   <title><?php bloginfo('name'); ?> | <?php if (is_404()) {echo '404';}; single_post_title(); ?></title>
+   <meta charset="<?php bloginfo('charset'); ?>">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+   <meta name="author" content="Nikita Suchkov">
+   <meta name="website" content="https://github.com/suchkovcode">
+   <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/static/favicon.ico">
+   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/static/favicon-16x16.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/static/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="48x48" href="<?php echo get_template_directory_uri(); ?>/assets/static/favicon-48x48.png">
+   <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/static/manifest.webmanifest">
+   <meta name="mobile-web-app-capable" content="yes">
+   <meta name="theme-color" content="#fff">
+   <meta name="application-name" content="prduct.com">
+   <meta name="msapplication-TileColor" content="#fff">
+   <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/static/mstile-144x144.png">
+   <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/static/browserconfig.xml">
+   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+<?php
+}, 0);
+
+add_action('login_head', function () {
+   printf('<link rel="icon" type="image/png" sizes="16x16" href="%s">', get_theme_file_uri('/assets/static/favicon-16x16.png'));
+});
+
+add_action('admin_head', function () {
+   printf('<link rel="icon" type="image/png" sizes="16x16" href="%s">', get_theme_file_uri('/assets/static/favicon-16x16.png'));
+});
+
+add_action('wp_enqueue_scripts', function () {
+   wp_deregister_script('jquery');
+   wp_dequeue_style('wp-block-library');
+   wp_dequeue_style('wp-block-library-theme');
+   wp_dequeue_style('wc-block-style');
+   wp_dequeue_style('classic-theme-styles');
+});
+
+
+add_filter('post_row_actions', 'rd_duplicate_post_link', 10, 2);
+add_filter('page_row_actions', 'rd_duplicate_post_link', 10, 2);
+add_filter('wpcf7_autop_or_not', '__return_false');
+add_filter('wpcf7_form_elements', function ($content) {
+   $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+   return $content;
+});
+
+add_theme_support('post-thumbnails', array('post', 'page'));
+add_theme_support('automatic-feed-links');
+add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'script', 'style']);
+
+
+
+
+function rd_duplicate_post_link($actions, $post) {
+   if (current_user_can('edit_posts')) {
+      $actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=rd_duplicate_post_as_draft&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce') . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+   }
+   return $actions;
+}
+
+
+
+function html5_slash_fixer( $buffer ) {
+   return str_replace( ' />', '>', $buffer );
+}
+
+function html5_slash_fixer_flush() {
+   ob_end_flush();
+}
+
+add_action( 'template_redirect', function(){
+   ob_start( function( $buffer ){
+       $buffer = str_replace( array( 'type="text/javascript"', "type='text/javascript'" ), '', $buffer );
+       $buffer = str_replace( array( 'type="text/css"', "type='text/css'" ), '', $buffer );
+       return $buffer;
+   });
+});
+
+
---
+++ webpack.config.js
@@ -0,0 +1,210 @@
+/* eslint-disable indent */
+/* eslint-disable no-unused-vars */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+   target: isDev ? "web" : "browserslist",
+   devtool: isDev ? "eval-cheap-source-map" : undefined,
+   entry: {
+      app: path.resolve(__dirname, "./src/app.js"),
+   },
+   output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: isDev ? "[name].[contenthash].js" : "[name].min.js",
+      clean: true,
+   },
+   resolve: {
+      extensions: [".js", ".json", ".scss", ".css"],
+      alias: {
+         Root: path.resolve(__dirname, "./src"),
+         Assets: path.resolve(__dirname, "./src/assets"),
+         Components: path.resolve(__dirname, "./src/components"),
+      },
+   },
+   cache: {
+      type: "filesystem",
+      cacheDirectory: path.resolve(__dirname, ".cache"),
+      idleTimeoutAfterLargeChanges: 3000,
+      maxAge: 172800000,
+      maxMemoryGenerations: isDev ? 5 : Infinity,
+      memoryCacheUnaffected: true,
+   },
+   optimization: {
+      splitChunks: {
+         cacheGroups: {
+            styles: {
+               name: "styles",
+               type: "css/mini-extract",
+               chunks: "all",
+               enforce: true,
+            },
+         },
+      },
+      minimizer: [
+         "...",
+         new ImageMinimizerPlugin({
+            test: /\.(jpe?g|png|gif)$/i,
+            exclude: /(node_modules|bower_components)/,
+            minimizer: {
+               implementation: ImageMinimizerPlugin.imageminMinify,
+               options: {
+                  plugins: [
+                  ["gifsicle", { interlaced: true, optimizationLevel: 2}],
+                  ["mozjpeg", { quality: 50, progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }],
+                  ["optipng", { optimizationLevel: 5 }],
+               ],
+             },
+           },
+         }),
+      ],
+   },
+   performance: {
+      hints: false,
+      maxAssetSize: 100000,
+      maxEntrypointSize: 400000,
+   },
+   devServer: {
+      static: {
+         directory: path.join(__dirname, "./dist"),
+      },
+      devMiddleware: {
+         serverSideRender: false,
+         writeToDisk: false,
+      },
+      server: "http",
+      compress: false,
+      http2: false,
+      https: false,
+      port: 5500,
+      open: true,
+      hot: true,
+      liveReload: false,
+   },
+   externals: {
+      jquery: "jQuery",
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         filename: "index.html",
+         template: path.resolve(__dirname, "./src/pages/index.html"),
+         inject: "body",
+         scriptLoading: "blocking",
+         xhtml: false,
+      }),
+      new MiniCssExtractPlugin({
+         filename: isDev ? "style.css" : "style.css",
+         linkType: "text/css",
+      }),
+      new FaviconsWebpackPlugin({
+         logo: "./src/assets/img/png/favicon.png",
+         cache: true,
+         mode: "auto",
+         publicPath: "./assets/static",
+         outputPath: "./assets/static",
+         prefix: "",
+         inject: true,
+         favicons: {
+            appName: "example",
+            appDescription: "example",
+            developerName: "example",
+            appShortName: "example",
+            developerURL: "example",
+            display: "standalone",
+            background: "#fff",
+            theme_color: "#fff",
+            lang: "en-US",
+            start_url: "/",
+            icons: {
+               android: true,
+               appleIcon: false,
+               appleStartup: false,
+               favicons: true,
+               windows: true,
+               yandex: false,
+            },
+         },
+      }),
+      new CopyPlugin({
+         patterns: [
+            {
+               from: "./src/other/screenshot.png",
+               toType: "dir",
+               force: true,
+            },
+            {
+               from: "./src/other/functions.php",
+               toType: "dir",
+               force: true,
+            },
+            {
+               from: "./src/assets/img/svg/sprites.svg",
+               to: "assets/img/",
+               toType: "dir",
+               force: true,
+            },
+         ],
+      }),
+   ],
+   module: {
+      rules: [
+         {  // Start Babel
+            test: /\.(js|jsx)$/,
+            include: path.resolve(__dirname, "./src"),
+            exclude: /(node_modules|bower_components)/,
+            use: isDev ? undefined : {
+               loader: "babel-loader",
+               options: {
+                  presets: ["@babel/preset-env"]
+               }
+            }
+         }, // End Babel
+
+         {  // Start Html
+            test: /\.html$/i,
+            exclude: /(node_modules|bower_components)/,
+            include: path.resolve(__dirname, "./src"),
+            loader: "html-loader",
+            options: {
+               minimize: isDev ? false : false,
+              },
+         }, // End Html
+
+         {  // Start Scss
+            test: /\.s[ac]ss$/i,
+            exclude: /(node_modules|bower_components)/,
+            include: path.resolve(__dirname, "./src"),
+            use: [
+               isDev ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader",
+            ],
+         }, // End Scss
+
+         {  // Start IMG
+            test: /\.(jpg|png|svg|jpeg|gif|webp)$/,
+            exclude: /(node_modules|bower_components)/,
+            include: path.resolve(__dirname, "./src"),
+            type: "asset/resource",
+            generator: {
+               filename: isDev ? "assets/img/[contenthash][ext]" : "assets/img/[name][ext]",
+            },
+         }, // End IMG
+
+
+         {  // Start Fonts
+            test: /\.(eot|ttf|woff|woff2)$/i,
+            exclude: /(node_modules|bower_components)/,
+            include: path.resolve(__dirname, "./src"),
+            type: "asset/resource",
+            generator: {
+               filename: "assets/fonts/[name][ext]",
+            },
+         }, // End Fonts
+      ],
+   },
+};
